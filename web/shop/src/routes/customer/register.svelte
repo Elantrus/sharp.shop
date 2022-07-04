5d12a6f035275a6a1fda66a872c9ddf5afd37dac
DrokeRavens <droke.ravens@gmail.com>
2022-07-04 19:43:13 -0300

    register page and api communication working

--- web/shop/src/routes/customer/register.svelte
+++ web/shop/src/routes/customer/register.svelte
@@ -1,8 +1,21 @@
 <script lang="ts">
-    import type { CreateCustomer} from '../../models/customer/register'
     import Input from '../../components/ui/forms/input.svelte'
     import {CustomerService} from '../../services/customer'
+    import {notifications} from '../../stores/notificationStore';
+    import { AuthenticationService } from '../../services/authentication';
+    import { credentials } from '../../stores/credentialsStore';
 
+    //Models
+    import type { CreateCustomer} from '../../models/customer/register'
+    import type {AuthenticationResult} from '../../models/authentication/credentials'
+    import type {CustomerAuthentication} from '../../models/customer/login'
+    
+    import {page} from "$app/stores";
+    import { goto } from '$app/navigation';
+    import {visitor} from '../../stores/visitorStore'
+
+    export let query : URLSearchParams = $page.url.searchParams;
+    
     let createCustomerModel : CreateCustomer = {
        email : '',
        name: '',
@@ -30,7 +43,23 @@
     };
 
     async function register(){
-        CustomerService.create(createCustomerModel)
+        CustomerService.create(createCustomerModel).then(response => {
+            const loginCredentials : CustomerAuthentication = {
+                email: createCustomerModel.email,
+                password: createCustomerModel.password,
+                deviceId: $visitor
+            };
+
+            AuthenticationService.login(loginCredentials).then(authResponse => {
+                const data : AuthenticationResult = authResponse.data;
+                credentials.set(data);
+
+                let callBack = query.get("callback");
+
+                goto(callBack ? callBack : "/");
+            }).catch(error => notifications.danger(error, 300))
+
+        }).catch(error => notifications.danger(error, 3000));
     }
  
 </script>
@@ -42,9 +71,9 @@
     </div>
     <form on:submit|preventDefault={register} class="flex flex-col gap-2 w-full">
         
-        <Input bind:value={createCustomerModel.password} type="text" bind:isValid={formValidation["name"].valid}>Name</Input>
+        <Input bind:value={createCustomerModel.name} type="text" bind:isValid={formValidation["name"].valid}>Name</Input>
 
-        <Input bind:value={createCustomerModel.password} type="text" bind:isValid={formValidation["surName"].valid}>Surname</Input>
+        <Input bind:value={createCustomerModel.surName} type="text" bind:isValid={formValidation["surName"].valid}>Surname</Input>
 
         <Input bind:value={createCustomerModel.email} type="email" bind:isValid={formValidation["email"].valid}>E-mail</Input>
 
